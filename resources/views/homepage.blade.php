2896743cd312ff709c705bde3da4058a7ad5a04f
Rob1nHoed <94637866+Rob1nHoed@users.noreply.github.com>
2023-02-22 11:49:11 +0100

    Zoekbalk toegevoegd

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -2,7 +2,23 @@
     <div class="py-12">
 
         {{-- Zoekbalk --}}
-        {{-- todo --}}
+        <div class="max-w-5xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    <div class="flex justify-center">
+                        <div class="w-3/4">
+                            <form action="{{ route('show.searchResult') }}" method="POST">
+                                @csrf
+                                <div class="flex items">
+                                    <input type="text" name="name" class="w-full rounded border shadow p-2 mr-2 focus:outline-none focus:shadow-outline" placeholder="Search for cocktails" required>
+                                    <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Search</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
         
         {{-- 8 willekeurige featured dranken --}}
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 pt-20">
